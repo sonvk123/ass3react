608485d4b4b7e756fbde29f4a1c0d5b719eb3371
Sơn <70042474+sonvk123@users.noreply.github.com>
2023-09-28 08:01:10 +0700

    Updates

---
+++ precache-manifest.9dffefb32b6c25290edb1edbcc92d473.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "040471c5bbfea00c4996",
+    "url": "/ass3react/static/css/main.06f04b1c.chunk.css"
+  },
+  {
+    "revision": "040471c5bbfea00c4996",
+    "url": "/ass3react/static/js/main.193bbcd6.chunk.js"
+  },
+  {
+    "revision": "c9bc3dd92dc387ae7cf5",
+    "url": "/ass3react/static/js/runtime~main.a7ee2a11.js"
+  },
+  {
+    "revision": "0675fe1944ba69bb8987",
+    "url": "/ass3react/static/css/2.5afb9c60.chunk.css"
+  },
+  {
+    "revision": "0675fe1944ba69bb8987",
+    "url": "/ass3react/static/js/2.9471eefd.chunk.js"
+  },
+  {
+    "revision": "93b7003b8b3fe8bac328",
+    "url": "/ass3react/static/js/3.c17d3b5c.chunk.js"
+  },
+  {
+    "revision": "67addf5b6a78eee6c62fc0934e9b4fb5",
+    "url": "/ass3react/static/media/caret-left-solid.67addf5b.svg"
+  },
+  {
+    "revision": "fd82da797f1b6f703b0931d0b8d0b3c0",
+    "url": "/ass3react/static/media/caret-right-solid.fd82da79.svg"
+  },
+  {
+    "revision": "3ea3fe2d4d3ee073b5bca4bfeaa0620a",
+    "url": "/ass3react/static/media/banner1.3ea3fe2d.jpg"
+  },
+  {
+    "revision": "2226e8c5b8bb36e71e85efe6ca56dcd3",
+    "url": "/ass3react/static/media/product_1.2226e8c5.png"
+  },
+  {
+    "revision": "050782eff4f52d2e237ef92db54ee439",
+    "url": "/ass3react/static/media/product_2.050782ef.png"
+  },
+  {
+    "revision": "e852e55d7a1a1c19cf380ab7db6bcc74",
+    "url": "/ass3react/static/media/product_3.e852e55d.png"
+  },
+  {
+    "revision": "261d3324f210f9e413a6e30a8adb48c4",
+    "url": "/ass3react/static/media/product_4.261d3324.png"
+  },
+  {
+    "revision": "d5c0e34a9663f1b4e9fa8807f60bc108",
+    "url": "/ass3react/static/media/product_5.d5c0e34a.png"
+  },
+  {
+    "revision": "594cab9ebefdf83233ef20ce97dbea6a",
+    "url": "/ass3react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ass3react/precache-manifest.9dffefb32b6c25290edb1edbcc92d473.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ass3react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
